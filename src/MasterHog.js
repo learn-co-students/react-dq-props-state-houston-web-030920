e7edfe3b3e7b8cf37642474d0e5fefbe072ba556
Rooted1 <ruthobey@gmail.com>
2020-08-30 17:13:20 -0500

    Done.

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -24,7 +24,7 @@ export default class MasterHog extends Component {
     return (
       <div>
 
-        <input type="radio" name="eyeColor" value="blue" onChange={this.changeEyeColor} />
+        <input type="radio" name="eyeColor" value="blue" onChange={this.changeEyeColor} checked />
         Blue<br></br>
         <input type="radio" name="eyeColor" value="sun" onChange={this.changeEyeColor} />
         Sun<br></br>
